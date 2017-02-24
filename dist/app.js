5dcafd8721bb5c3b73e0c0d3e0ac562b47dcd964
Matthew McCord <mattm.eternal@gmail.com>
2017-02-23 22:05:56 -0600

    compiled with grunt

--- dist/app.js
+++ dist/app.js
@@ -224,10 +224,7 @@ module.exports = BattleBots;
 },{"./RobotModels.js":3}],3:[function(require,module,exports){
 "use strict";
 
-let Models = {},
-		loc = window.location.pathname;
-
-const CURRENT_DIRECTORY = loc.substring(0, loc.lastIndexOf('/'));
+let Models = {};
 
 let RobotModel = function() {
   this.name = "ketchup robot";
@@ -244,24 +241,24 @@ MiniTank.prototype = new RobotModel();
 Models.Johnny5 = function(){
 	this.lowHealth = 50;
 	this.highHealth = 200;
-	this.health = (Math.floor(Math.random() * (this.highHealth - this.lowHealth + 1)) + this.lowHealth) + 150;
+	this.health = Math.floor(Math.random() * (this.highHealth - this.lowHealth + 1)) + this.lowHealth;
   this.lowDamage = 30;
   this.highDamage = 60;
 	this.name = "Johnny5";
 	this.skill = "Heat Vision";
-	this.url = `${CURRENT_DIRECTORY}/res/Johnny5.jpg`;
+	this.url = "./../res/johnny5.jpg";
 };
 Models.Johnny5.prototype = new MiniTank();
 
 Models.WallE = function(){
 	this.lowHealth = 100;
 	this.highHealth = 160;
-	this.health = (Math.floor(Math.random() * (this.highHealth - this.lowHealth + 1)) + this.lowHealth) + 150;
+	this.health = Math.floor(Math.random() * (this.highHealth - this.lowHealth + 1)) + this.lowHealth;
   this.lowDamage = 35;
   this.highDamage = 38;
 	this.name = "Wall-E";
 	this.skill = "Adoreableness";
-	this.url = `${CURRENT_DIRECTORY}/res/WallE.jpg`;
+	this.url = "./../res/walle.jpg";
 };
 Models.WallE.prototype = new MiniTank();
 
@@ -275,24 +272,24 @@ Tripod.prototype = new RobotModel();
 Models.R2D2 = function(){
 	this.lowHealth = 125;
 	this.highHealth = 160;
-	this.health = (Math.floor(Math.random() * (this.highHealth - this.lowHealth + 1)) + this.lowHealth) + 150;
+	this.health = Math.floor(Math.random() * (this.highHealth - this.lowHealth + 1)) + this.lowHealth;
   this.lowDamage = 5;
   this.highDamage = 80;
 	this.name = "R2D2";
 	this.skill = "Soldering Iron";
-	this.url = `${CURRENT_DIRECTORY}/res/R2D2.jpg`;
+	this.url = './../res/r2d2.jpg';
 };
 Models.R2D2.prototype = new Tripod();
 
 Models.R2BRO2 = function(){
 	this.lowHealth = 80;
 	this.highHealth = 180;
-	this.health = (Math.floor(Math.random() * (this.highHealth - this.lowHealth + 1)) + this.lowHealth) + 150;
+	this.health = Math.floor(Math.random() * (this.highHealth - this.lowHealth + 1)) + this.lowHealth;
   this.lowDamage = 15;
   this.highDamage = 70;
 	this.name = "R2BRO2";
 	this.skill = "Keg Chuck";
-	this.url = `${CURRENT_DIRECTORY}/res/R2BRO2.jpg`;
+	this.url = './../res/r2bro2.jpg';
 };
 Models.R2BRO2.prototype = new Tripod();
 
@@ -306,24 +303,24 @@ Bipedal.prototype = new RobotModel();
 Models.Bender = function(){
 	this.lowHealth = 70;
 	this.highHealth = 190;
-	this.health = (Math.floor(Math.random() * (this.highHealth - this.lowHealth + 1)) + this.lowHealth) + 150;
+	this.health = Math.floor(Math.random() * (this.highHealth - this.lowHealth + 1)) + this.lowHealth;
   this.lowDamage = 20;
   this.highDamage = 70;
 	this.name = "Bender";
 	this.skill = "Bending";
-	this.url = `${CURRENT_DIRECTORY}/res/Bender.jpg`;
+	this.url = './../res/bender.jpg';
 };
 Models.Bender.prototype = new Bipedal();
 
 Models.BayMax = function(){
 	this.lowHealth = 40;
 	this.highHealth = 230;
-	this.health = (Math.floor(Math.random() * (this.highHealth - this.lowHealth + 1)) + this.lowHealth) + 150;
+	this.health = Math.floor(Math.random() * (this.highHealth - this.lowHealth + 1)) + this.lowHealth;
   this.lowDamage = 25;
   this.highDamage = 50;
 	this.name = "BayMax";
 	this.skill = "Anti Heal";
-	this.url = `${CURRENT_DIRECTORY}/res/BayMax.jpg`;
+	this.url = './../res/baymax.jpg';
 };
 Models.BayMax.prototype = new Bipedal();
 
